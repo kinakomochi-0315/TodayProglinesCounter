d7fbde7a5eea7151d63a0cf57c5b72b1a051d298
Riku Murooka <22022042@hiroshima.kosen-ac.jp>
2024-12-02 15:10:56 +0900

    Initial commit

---
+++ TodayProglinesCounter/GithubLogin.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TodayProglinesCounter;
+
+public partial class GithubLogin : Window
+{
+    public GithubLogin()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TodayProglinesCounter/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace TodayProglinesCounter;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
